89f7f5356564ab3eeddb97b93b35809b34f9677c
ZHAO Shuai <roy.zhao@essilorchina.com>
2020-03-19 13:28:31 +0800

    Add project files.

---
+++ Webslots/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webslots.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
